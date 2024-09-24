51c868bf4ee39e070448914d2bf6fee61a437e7f
Marek Vospěl <marek@vospel.cz>
2024-09-24 17:11:33 +0200

    fix: body newline problem, more tests

--- src/parse_req.c
+++ src/parse_req.c
@@ -4,33 +4,46 @@
 
 #include "../include/parse_req.h"
 
+void _set_state(struct parse_state *pstate, uint8_t next) {
+  pstate->parse_state = next;
+  pstate->last_newline = false;
+  pstate->parse_index = 0;
+}
+
 void parse_cycle(struct parse_state *pstate, uint8_t *buf,
                  uint_fast32_t bufsize) {
+
   for (int i = 0; i < bufsize; i += 1) {
 
     if (buf[i] == '\n') {
       if (pstate->parse_state <= 2) {
+
+        // HTTP header
         if (pstate->parse_state == 0) {
           pstate->request->method[pstate->parse_index] = '\0';
         } else if (pstate->parse_state == 1) {
           pstate->request->path[pstate->parse_index] = '\0';
         }
 
-        pstate->parse_state = 3;
-        pstate->parse_index = 0;
-        pstate->last_newline = false;
+        _set_state(pstate, 3);
         continue;
       }
+      // Headers
       if (pstate->parse_state == 3 && pstate->last_newline) {
-        pstate->last_newline = false;
-        pstate->parse_index = 0;
-        pstate->parse_state = 4;
+        _set_state(pstate, 4);
         continue;
       }
+      // Body
       if (pstate->parse_state == 4 && pstate->last_newline) {
-        pstate->request->body[pstate->parse_index] = '\0';
+        // Remove the latest newline, therefore -1
+        int index = pstate->parse_index;
 
-        pstate->parse_state = 5;
+        if (index > 0) {
+          index -= 1;
+        }
+        pstate->request->body[pstate->parse_index - 1] = '\0';
+
+        _set_state(pstate, 5);
       }
       pstate->last_newline = true;
     } else {
@@ -40,14 +53,12 @@ void parse_cycle(struct parse_state *pstate, uint8_t *buf,
     if (buf[i] == ' ') {
       if (pstate->parse_state == 0) {
         pstate->request->method[pstate->parse_index] = '\0';
-        pstate->parse_index = 0;
-        pstate->parse_state = 1;
+        _set_state(pstate, 1);
         continue;
       }
       if (pstate->parse_state == 1) {
         pstate->request->path[pstate->parse_index] = '\0';
-        pstate->parse_index = 0;
-        pstate->parse_state = 2;
+        _set_state(pstate, 2);
         continue;
       }
     }
--- test/parse_test.c
+++ test/parse_test.c
@@ -4,29 +4,68 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-Test(simple, test) {
-
+void with_request(void (*fn)(struct parse_state *)) {
   struct http_req req;
   struct parse_state pstate;
 
   cr_assert_eq(initialize_request(&req), 0);
   initialize_parse_state(&pstate, &req);
 
-  uint8_t buf[] = "GET /abc/def HTTP/1.0\nAccept: */*\nContent-Type: "
-                  "application/text\n\nzkouska123\n\n";
-
-  parse_cycle(&pstate, (uint8_t *)&buf, sizeof(buf));
-
-  printf("'%s' %i\n", req.method, strcmp(req.method, "GET"));
-  printf("'%s' %i\n", req.path, strcmp(req.path, "/abc/def"));
-  printf("'%s' %i\n", req.body, strcmp(req.body, "zkouska123"));
-
-  cr_assert(strcmp(req.method, "GET") == 0);
-  cr_assert(strcmp(req.path, "/abc/def") == 0);
-  // TODO: wrong value, body shouldn't have the last newline
-  cr_assert(strcmp(req.body, "zkouska123\n") == 0);
+  // Test
+  (*fn)(&pstate);
 
   free(req.method);
   free(req.path);
   free(req.body);
 }
+
+void request_get(struct parse_state *pstate) {
+  uint8_t buf[] = "GET /abc/def HTTP/1.0\nAccept: */*\nContent-Type: "
+                  "application/text\n\nzkouska123\n\n";
+
+  parse_cycle(pstate, (uint8_t *)&buf, sizeof(buf));
+
+  printf("'%s'\n", pstate->request->method);
+  printf("'%s'\n", pstate->request->path);
+  printf("'%s'\n", pstate->request->body);
+
+  cr_assert(strcmp(pstate->request->method, "GET") == 0);
+  cr_assert(strcmp(pstate->request->path, "/abc/def") == 0);
+  cr_assert(strcmp(pstate->request->body, "zkouska123") == 0);
+}
+
+void request_emptybody(struct parse_state *pstate) {
+  uint8_t buf[] = "POST /index.html HTTP/1.0\nUser-Agent: "
+                  "C22\nX-Forwarded-For: 127.0.0.1\nHost: vospel.cz\n\n\n\n";
+
+  parse_cycle(pstate, (uint8_t *)&buf, sizeof(buf));
+
+  printf("'%s'\n", pstate->request->method);
+  printf("'%s'\n", pstate->request->path);
+  printf("'%s'\n", pstate->request->body);
+
+  cr_assert(strcmp(pstate->request->method, "POST") == 0);
+  cr_assert(strcmp(pstate->request->path, "/index.html") == 0);
+  cr_assert(strcmp(pstate->request->body, "") == 0);
+}
+
+void multi_iter(struct parse_state *pstate) {
+  uint8_t buf[] =
+      "GET /index.css HTTP/1.0\nA: B\nC: D\nE: false\nF: true\n\n\n\n";
+
+  for (int i = 0; i < sizeof(buf); i++) {
+    parse_cycle(pstate, (uint8_t *)&(buf[i]), 1);
+  }
+
+  printf("'%s'\n", pstate->request->method);
+  printf("'%s'\n", pstate->request->path);
+  printf("'%s'\n", pstate->request->body);
+
+  cr_assert(strcmp(pstate->request->method, "GET") == 0);
+  cr_assert(strcmp(pstate->request->path, "/index.css") == 0);
+  cr_assert(strcmp(pstate->request->body, "") == 0);
+}
+
+Test(request, get) { with_request(&request_get); }
+Test(request, empty) { with_request(&request_emptybody); }
+Test(request, multiiter) { with_request(&multi_iter); }
